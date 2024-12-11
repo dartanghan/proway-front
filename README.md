655e5d512a1210e130b3640ca3066035245b836c
Dartanghan Vani <dartanghan.vani@recuperi.com.br>
2024-12-11 16:52:02 -0300

    feat: Prim

---
+++ README.md
@@ -0,0 +1,33 @@
+# proway-front
+Neste projeto usamos o NPM 20 para executar
+
+Como foi desenvolvido:
+Usamos a imagem nodejs:20
+Precisamos exportar a porta 8081
+
+
+
+
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  Bem vindo ao sistema de alunos Proway
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required>
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required>
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      fetch('http://localhost:8080/api-auth/login/', {
+        method: 'POST',        
+        headers: {
+          'Content-Type': 'application/json',
+          'Origin': 'http://localhost:8081'
+        },
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password
+        })
+      })
+      .then(response => {
+        console.log(response.status)
+        if( response.status == '403' ){
+          alert('Comunicacao funcional')
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
